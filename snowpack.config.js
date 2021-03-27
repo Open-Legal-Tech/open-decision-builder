af2c4791f7a9b5c87eb0046edc1e427f08888681
PhilGarb <phil.garbowsky@philtech.dev>
2021-03-27 13:26:07 +0100

    ci: :wrench: Eslint ignore node_modules

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: [
     "@snowpack/plugin-react-refresh",
     // "@snowpack/plugin-typescript",
-    "@canarise/snowpack-eslint-plugin",
+    // "@canarise/snowpack-eslint-plugin",
     [
       "@snowpack/plugin-build-script",
       {
