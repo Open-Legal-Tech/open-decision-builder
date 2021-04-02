667eb7c77630d164c792ffa5c424c6cf638bf22c
PhilGarb <phil.garbowsky@philtech.dev>
2021-04-02 15:02:18 +0200

    ci: :bug: Fixed failing postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,3 @@
-
-
 module.exports = {
-  plugins: ['@tailwindcss/jit', 'autoprefixer', 'postcssPresetEnv'],
+  plugins: [require('@tailwindcss/jit'), require('autoprefixer'), require('postcss-preset-env')],
 };
