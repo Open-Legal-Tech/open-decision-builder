53c306fda7cf569b8a0deb2e07eb0a6ae8195089
PhilGarb <phil.garbowsky@philtech.dev>
2021-04-09 18:16:40 +0200

    refactor: :recycle: added missing lines

--- src/features/Builder/components/Stage/Stage.tsx
+++ src/features/Builder/components/Stage/Stage.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { useGesture } from "react-use-gesture";
 import { CSS, styled } from "utils/stitches.config";
+const StageContainer = styled("div", {
+  overflow: "hidden",
+  position: "relative",
+  outline: "none",
+  backgroundColor: "#ffffff",
+  backgroundImage:
+    "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%23000000' fill-opacity='0.05'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E\")",
+});
 
 type StageProps = {
   /**
@@ -78,11 +86,7 @@ export const Stage: Stage = ({
         }}
       >
         {/* This inner wrapper is used to zoom.  */}
-        <div className="absolute" style={{ transform: `scale(${zoom})` }}>
-          {children}
-        </div>
       </div>
-    </div>
     </StageContainer>
   );
 };
