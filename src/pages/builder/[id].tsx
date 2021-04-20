5c30b35866b1d74d5581a08f6b883973695c7f6d
Philipp <Phil.garbowsky@philtech.dev>
2021-04-20 13:37:36 +0200

    refactor: :heavy_plus_sign: Added basic react-flow implementation

--- src/pages/builder/[id].tsx
+++ src/pages/builder/[id].tsx
@@ -9,10 +9,42 @@ import {
 } from "features/Builder/tests/nodes";
 import { useTreeQuery } from "internalTypes";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import ReactFlow, { Background, Controls, MiniMap } from "react-flow-renderer";
+
+const initialElements = [
+  {
+    id: "1",
+    type: "input", // input node
+    data: { label: "Input Node" },
+    position: { x: 250, y: 25 },
+  },
+  // default node
+  {
+    id: "2",
+    // you can also pass a React component as a label
+    data: { label: <div>Default Node</div> },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: "3",
+    type: "output", // output node
+    data: { label: "Output Node" },
+    position: { x: 250, y: 250 },
+  },
+  // animated edge
+  { id: "e1-2", source: "1", target: "2", animated: true },
+  { id: "e2-3", source: "2", target: "3" },
+];
 
 export default function Tree() {
   const [state] = useService(authService);
   const router = useRouter();
+
+  const [elements, setElements] = useState(initialElements);
+  const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+  const onConnect = (params) => setElements((els) => addEdge(params, els));
 
   const tree = useTreeQuery<TreeType>(
     state.context.client,
@@ -32,7 +64,31 @@ export default function Tree() {
 
   return (
     <MainContent css={{ overflow: "hidden" }}>
-      {tree.isSuccess && <NodeEditor tree={tree.data} />}
+      {/* {tree.isSuccess && <NodeEditor tree={tree.data} />} */}
+      <ReactFlow
+        elements={elements}
+        onElementsRemove={onElementsRemove}
+        onConnect={onConnect}
+        deleteKeyCode={46}
+      >
+        <Background variant="lines" gap={12} size={1} />
+        <MiniMap
+          nodeColor={(node) => {
+            switch (node.type) {
+              case "input":
+                return "red";
+              case "default":
+                return "#00ff00";
+              case "output":
+                return "rgb(0,0,255)";
+              default:
+                return "#eee";
+            }
+          }}
+          nodeStrokeWidth={3}
+        />
+        <Controls />
+      </ReactFlow>
     </MainContent>
   );
 }
