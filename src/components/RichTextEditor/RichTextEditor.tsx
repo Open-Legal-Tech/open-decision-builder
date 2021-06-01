050c8d8cf999a8b25a1ba72451d1dee664ba0cbf
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2021-06-01 19:31:06 +0200

    fix: :bug: Removed BallonToolbar because it caused an overflow

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import {
-  BalloonToolbar,
   createAlignPlugin,
   createBlockquotePlugin,
   createBoldPlugin,
-  createCodeBlockPlugin,
-  createCodePlugin,
   createExitBreakPlugin,
   createHeadingPlugin,
   createHistoryPlugin,
@@ -28,13 +25,6 @@ import {
   ELEMENT_ALIGN_RIGHT,
   ELEMENT_BLOCKQUOTE,
   ELEMENT_CODE_BLOCK,
-  ELEMENT_CODE_LINE,
-  ELEMENT_H1,
-  ELEMENT_H2,
-  ELEMENT_H3,
-  ELEMENT_H4,
-  ELEMENT_H5,
-  ELEMENT_H6,
   ELEMENT_IMAGE,
   ELEMENT_MEDIA_EMBED,
   ELEMENT_OL,
@@ -47,18 +37,11 @@ import {
   isBlockAboveEmpty,
   isSelectionAtBlockStart,
   KEYS_HEADING,
-  MARK_BOLD,
-  MARK_CODE,
-  MARK_ITALIC,
-  MARK_STRIKETHROUGH,
-  MARK_UNDERLINE,
-  MediaEmbedElement,
   SlatePlugins,
   ToolbarAlign,
   ToolbarImage,
   ToolbarLink,
   ToolbarList,
-  ToolbarMark,
   useEventEditorId,
   useStoreEditorRef,
 } from "@udecode/slate-plugins";
@@ -67,11 +50,8 @@ import {
   MdFormatAlignJustify,
   MdFormatAlignLeft,
   MdFormatAlignRight,
-  MdFormatBold,
-  MdFormatItalic,
   MdFormatListBulleted,
   MdFormatListNumbered,
-  MdFormatUnderlined,
   MdImage,
   MdLink,
 } from "react-icons/md";
@@ -198,143 +178,19 @@ export const RichTextEditor = (): JSX.Element => {
     );
   };
 
-  const BallonToolbarMarks = () => {
-    const editor = useStoreEditorRef(useEventEditorId("focus"));
-
-    const arrow = false;
-    const theme = "dark";
-    const direction = "top";
-    const hiddenDelay = 0;
-    const tooltip: any = {
-      arrow: true,
-      delay: 0,
-      duration: [200, 0],
-      hideOnClick: false,
-      offset: [0, 17],
-      placement: "top",
-    };
-
-    return (
-      <BalloonToolbar
-        direction={direction}
-        hiddenDelay={hiddenDelay}
-        theme={theme}
-        arrow={arrow}
-      >
-        <ToolbarMark
-          type={getSlatePluginType(editor, MARK_BOLD)}
-          icon={<MdFormatBold />}
-        />
-        <ToolbarMark
-          type={getSlatePluginType(editor, MARK_ITALIC)}
-          icon={<MdFormatItalic />}
-        />
-        <ToolbarMark
-          type={getSlatePluginType(editor, MARK_UNDERLINE)}
-          icon={<MdFormatUnderlined />}
-        />
-      </BalloonToolbar>
-    );
-  };
-
   return (
-    <>
-      <HeadingToolbar>
+    <div>
+      <HeadingToolbar styles={{ root: { padding: "0", margin: "0" } }}>
         <ToolbarButtonsAlign />
         <ToolbarImage icon={<MdImage />} /> <ToolbarLink icon={<MdLink />} />
         <ToolbarButtonsList />
       </HeadingToolbar>
-      <BallonToolbarMarks />
       <SlatePlugins
         editableProps={editableProps}
-        initialValue={initialValueBasicElements}
         components={components}
         options={options}
         plugins={pluginsBasic}
       />
-    </>
+    </div>
   );
 };
-
-export const createElement = (
-  text = "",
-  {
-    type = ELEMENT_PARAGRAPH,
-    mark,
-  }: {
-    type?: string;
-    mark?: string;
-  } = {}
-) => {
-  const leaf: any = { text };
-  if (mark) {
-    leaf[mark] = true;
-  }
-
-  return {
-    type,
-    children: [leaf],
-  };
-};
-
-const initialValueBasicElements = [
-  createElement("🧱 Elements", { type: ELEMENT_H1 }),
-  createElement("🔥 Basic Elements", { type: ELEMENT_H2 }),
-  createElement("These are the most common elements, known as blocks:"),
-  createElement("Heading 1", { type: ELEMENT_H1 }),
-  createElement("Heading 2", { type: ELEMENT_H2 }),
-  createElement("Heading 3", { type: ELEMENT_H3 }),
-  createElement("Heading 4", { type: ELEMENT_H4 }),
-  createElement("Heading 5", { type: ELEMENT_H5 }),
-  createElement("Heading 6", { type: ELEMENT_H6 }),
-  createElement("Blockquote", { type: ELEMENT_BLOCKQUOTE }),
-  {
-    type: ELEMENT_CODE_BLOCK,
-    children: [
-      {
-        type: ELEMENT_CODE_LINE,
-        children: [
-          {
-            text: "const a = 'Hello';",
-          },
-        ],
-      },
-      {
-        type: ELEMENT_CODE_LINE,
-        children: [
-          {
-            text: "const b = 'World';",
-          },
-        ],
-      },
-    ],
-  },
-  createElement("💅 Marks", { type: ELEMENT_H1 }),
-  createElement("💧 Basic Marks", { type: ELEMENT_H2 }),
-  createElement(
-    "The basic marks consist of text formatting such as bold, italic, underline, strikethrough, subscript, superscript, and code."
-  ),
-  createElement(
-    "You can customize the type, the component and the hotkey for each of these."
-  ),
-  createElement("This text is bold.", { mark: MARK_BOLD }),
-  createElement("This text is italic.", { mark: MARK_ITALIC }),
-  createElement("This text is underlined.", {
-    mark: MARK_UNDERLINE,
-  }),
-  {
-    type: ELEMENT_PARAGRAPH,
-    children: [
-      {
-        text: "This text is bold, italic and underlined.",
-        [MARK_BOLD]: true,
-        [MARK_ITALIC]: true,
-        [MARK_UNDERLINE]: true,
-      },
-    ],
-  },
-  createElement("This is a strikethrough text.", {
-    mark: MARK_STRIKETHROUGH,
-  }),
-  createElement("This is an inline code.", { mark: MARK_CODE }),
-];
